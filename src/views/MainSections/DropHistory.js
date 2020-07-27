68890f2cf7864a4fc9cf15ff2759605f4f060e13
Damir Mansurov <rave9373@gmail.com>
2020-07-27 19:16:17 +0300

    initial commit

---
+++ src/views/MainSections/DropHistory.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { UncontrolledAlert } from "reactstrap";
+
+class DropHistory extends React.Component {
+    render() {
+      return (
+          <div className="drop-hero-block">
+          </div>
+      );
+    }
+}
+
+export default DropHistory;
